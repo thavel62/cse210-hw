1f5e142c9a96978434659bbc814e633a06b6f655
havelt <hav17005@byui.edu>
2024-04-24 10:21:47 -0600

    finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // User given magic number
+        // Console.WriteLine("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+
+        // Random Number Generator
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 50);
+    
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.WriteLine("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > magicNumber) 
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it! ");
+            }  
+        }
     }
 }
