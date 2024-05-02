d909cab64034a9409182a9d879947ddf9c48d206
havelt <hav17005@byui.edu>
2024-05-02 08:33:57 -0600

    Began journal program

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,5 +5,38 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop02 World!");
+
+        // Classes
+
+        // Journal.cs - Stores entries
+            // AddEntry(newEntry : Entry) :  void
+            // Display() : void
+            // SaveToFile(file : string)
+            // LoadFromFile(file : string)
+                // Attributes
+                // _entries : List<Entry>
+                // Methods
+                //AddEntry(newEntry : Entry) : void
+                // DisplayAll() : void
+                // SaveToFile(file : string) : void
+                // LoadFromFile(file : string) : void
+
+        // Entry.cs - Represents a single entry
+            // Display() : void
+                // Attributes
+                // _date : string
+                // _promptText : string
+                // _entryText : string
+                // Methods
+                // Display() : void
+
+        // PromptGenerator.cs - Generates random prompt
+            // GetRandomPrompt() : string
+                // Attributes
+                // _prompts : List<string>
+                // Methods
+                // GetRandomPrompt() : string
+
+
     }
 }
